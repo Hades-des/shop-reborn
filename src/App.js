085a54408d298f7650f25ee1a442ef8100bedc19
Hades-des <chornnegr@gmail.com>
2022-05-03 19:29:43 +0300

    add project

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Items from "./components/Items";
+
+class App extends React.Component {
+  constructor(props) {
+  super(props)
+  this.state = {
+    orders:[],
+    items: [
+      {
+        id:1,
+        title:'Стул серый',
+        img:'chair-grey.jpg',
+        desc:'Lorem ipsum dolor sit amet,consectetur adioisicing',
+        category: 'chairs',
+        price: '49.99'
+      },
+      {
+        id:2,
+        title:'Стол',
+        img:'table.png',
+        desc:'Lorem ipsum dolor sit amet,consectetur adioisicing',
+        category: 'tables',
+        price: '149.00'
+      },
+      {
+        id:3,
+        title:'Диван',
+        img:'sofa.jpg',
+        desc:'Lorem ipsum dolor sit amet,consectetur adioisicing',
+        category: 'sofa',
+        price: '549.99'
+      },
+      {
+        id:4,
+        title:'Лампа',
+        img:'lamp.jpg',
+        desc:'Lorem ipsum dolor sit amet,consectetur adioisicing',
+        category: 'decor',
+        price: '25.00'
+      },
+      {
+        id:5,
+        title:'Стул белый',
+        img:'chair-white.jpg',
+        desc:'Lorem ipsum dolor sit amet,consectetur adioisicing',
+        category: 'chairs',
+        price: '49.99'
+      },
+      {
+        id:6,
+        title:'Декоративные цветы',
+        img:'vase.jpg',
+        desc:'Lorem ipsum dolor sit amet,consectetur adioisicing',
+        category: 'decor',
+        price: ''
+      }
+    ]
+  }
+  this.addToOrder = this.addToOrder.bind(this)
+}
+  render() {
+   return (
+    <div className="wrapper">
+    <Header  orders={this.state.orders}/>
+    <Items items ={this.state.items} onAdd={this.addToOrder}/>
+    <Footer />
+    </div>
+  );
+ }
+
+ addToOrder(item) {
+  this.setState({orders: [...this.state.orders, item]}, () =>{
+    console.log(this.state.orders)
+  } )
+ }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import {FaShoppingCart} from "react-icons/fa";
+import Order from './Order';
+
+export default function Header(props) {
+  let[cartOpen, setCartOpen] = useState(false)
+
+  return (
+    <header>
+        <div>
+            <span className='logo'>House Staff</span>
+            <ul className='nav'>
+              <li>Про нас</li>
+              <li>Контакты</li>
+              <li>Кабинет</li>
+            </ul>
+            <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`}/>
+
+            {cartOpen && (
+              <div className='shop-cart'>
+                {props.orders.map(el => (
+                  <Order key={el.id} item={el}/>
+                ))}
+              </div>
+            )}
+        </div>
+        <div className='presentation'></div>
+    </header>
+  )
+}
